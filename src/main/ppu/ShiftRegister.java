eb83682eea78aa66df2e37f104ddad091a698742
zqumsieh <zayd@qumsieh.net>
2020-03-19 10:37:05 -0700

    Ate bowser's big bean burrito

--- src/main/ppu/ShiftRegister.java
+++ src/main/ppu/ShiftRegister.java
@@ -7,12 +7,9 @@ public class ShiftRegister {
     private int value;
     private int max;
 
-    String bowsersBigBeanBurrito;
-
     public ShiftRegister(int size) {
         this.max  = (int) Math.pow(2, size);
         value     = 0;
-        bowsersBigBeanBurrito = Integer.toBinaryString(value);
     }
 
     // EFFECTS: right-shifts the register by n
@@ -21,19 +18,15 @@ public class ShiftRegister {
         while (value >= max) {
             value -= max;
         }
-
-        bowsersBigBeanBurrito = Integer.toBinaryString(value);
     }
 
     // EFFECTS: left-shifts the register by n
     public void shiftLeft(int n) {
         value /= Math.pow(2, n);
-        bowsersBigBeanBurrito = Integer.toBinaryString(value);
     }
 
     public void setNthBits(int beginning, int end, int newBits) {
         value = Util.maskNthBits(newBits, value, 0, beginning, end - beginning);
-        bowsersBigBeanBurrito = Integer.toBinaryString(value);
     }
 
     public int getValue() {
