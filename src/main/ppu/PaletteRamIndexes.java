4e0f2f457b10e256199b119267133b5fb0d40da2
zqumsieh <zayd@qumsieh.net>
2020-04-04 16:56:16 -0700

    Modified PaletteRamIndexes to not use class Address.java

--- src/main/ppu/PaletteRamIndexes.java
+++ src/main/ppu/PaletteRamIndexes.java
@@ -9,33 +9,50 @@ import model.Address;
 public class PaletteRamIndexes {
     // Constants
     protected static final int PALETTE_RAM_INDEXES_SIZE = Integer.parseInt("0200", 16);
-    protected static final int INITIAL_INDEX_VALUE      = Integer.parseInt("00",   16);
 
     // Fields
-    private Address[] indexes;
+    private int[] indexes;
 
     // MODIFIES: this
     // EFFECTS:  initializes all the palette ram indexes and sets up the mirrors.
-
     public PaletteRamIndexes() {
-        indexes = new Address[PALETTE_RAM_INDEXES_SIZE];
+        indexes = new int[PALETTE_RAM_INDEXES_SIZE];
         for (int i = 0; i < PALETTE_RAM_INDEXES_SIZE; i++) {
-            indexes[i] = new Address(INITIAL_INDEX_VALUE);
+            indexes[i] = 0;
         }
-
-        // Setting up mirrors
-        indexes[16] = indexes[0];
-        indexes[20] = indexes[4];
-        indexes[24] = indexes[8];
-        indexes[28] = indexes[12];
     }
 
+    // MODIFIES: this
+    // EFFECTS:  mirrors the pointer and writes the value into the indexes.
     public void writeMemory(int pointer, int value) {
-        indexes[pointer].setValue(value);
+        pointer = mirrorAddress(pointer);
+        indexes[pointer] = value;
     }
+
+    // EFFECTS: mirrors the pointer such that:
+    //          10 -> 00
+    //          14 -> 04
+    //          18 -> 08
+    //          1C -> 0C
+    private int mirrorAddress(int pointer) {
+        if (pointer == Integer.parseInt("10", 16)) {
+            return Integer.parseInt("00", 16);
+        }
+        if (pointer == Integer.parseInt("14", 16)) {
+            return Integer.parseInt("04", 16);
+        }
+        if (pointer == Integer.parseInt("18", 16)) {
+            return Integer.parseInt("08", 16);
+        }
+        if (pointer == Integer.parseInt("1C", 16)) {
+            return Integer.parseInt("0C", 16);
+        }
+        return pointer;
+    }
+
 
     // EFFECTS: returns the value in memory at indexes, taking into account n % 4 mirrors.
-    public Address readMemory(int pointer) {
+    public int readMemory(int pointer) {
         if (pointer % 4 == 0) {
             return indexes[0];
         }
@@ -43,11 +60,7 @@ public class PaletteRamIndexes {
         return indexes[pointer];
     }
 
-    public Address[] getIndexes() {
+    public int[] getIndexes() {
         return indexes;
-    }
-
-    public Address peekMemory(int pointer) {
-        return indexes[pointer];
     }
 }
